84e5da05877de26ce4c1a2bbb2aa7ebb6b81683f
Temitayo Ojo <Temitayo.v.ojo.25@dartmouth.edu>
2026-02-13 10:53:18 -0500

    dev test

--- src/Middlegg/llvm_parser.c
+++ src/Middlegg/llvm_parser.c
@@ -67,51 +67,54 @@ void printMap2(std::unordered_map<std::string, std::vector<LLVMValueRef>> *m) {
 bool common_subexpr(LLVMBasicBlockRef bb) {
 	bool ret = false;
 	std::unordered_map<std::string, LLVMValueRef> m; 
+	
 	for (LLVMValueRef instruction = LLVMGetFirstInstruction(bb); instruction; instruction = LLVMGetNextInstruction(instruction)) {
 
-		// before we add we find
 		std::string key = LLVMPrintValueToString(instruction);
 
 		if (LLVMGetInstructionOpcode(instruction) == LLVMAlloca) {
 			m[key] = instruction;
+		} else if (LLVMGetInstructionOpcode(instruction) == LLVMStore) {
+			// A store invalidates all loads from that address
+			LLVMValueRef store_addr = LLVMGetOperand(instruction, 1);
+			
+			// Remove all loads from this address from the map
+			std::vector<std::string> to_remove;
+			for (auto it = m.begin(); it != m.end(); it++) {
+				if (LLVMGetInstructionOpcode(it->second) == LLVMLoad) {
+					LLVMValueRef load_addr = LLVMGetOperand(it->second, LLVMGetNumOperands(it->second) - 1);
+					if (load_addr == store_addr) {
+						to_remove.push_back(it->first);
+					}
+				}
+			}
+			
+			for (auto& k : to_remove) {
+				m.erase(k);
+			}
+			
+			// Add the store itself
+			m[key] = instruction;
+			
 		} else {
-
+			// For other instructions, check for CSE
 			size_t pos = key.find('=');
 			if (pos != std::string::npos) {
 				key = key.substr(pos + 1);
 			}
 
 			if (m.count(key)) {
-				//printf("key exists '%s' in map\n", key.c_str());
 				LLVMReplaceAllUsesWith(instruction, m.at(key));
 				ret = true;
-				//puts("subexpr still made");
 			} else {
-				//printf("adding string '%s' in map\n", key.c_str());
-				if (LLVMGetInstructionOpcode(instruction) == LLVMStore) {
-					std::unordered_map<std::string, LLVMValueRef>::iterator it = m.begin();	
-					while(it != m.end()) {
-						if (LLVMGetInstructionOpcode(it->second) == LLVMLoad) {
-							int size = LLVMGetNumOperands(it->second);
-							LLVMValueRef cmp = LLVMGetOperand(it->second, size-1);
-							if (cmp == LLVMGetOperand(instruction, 1)) {
-								m.erase(it->first);
-								break;
-							}
-						}
-						it++;
-					}
-				}
 				m[key] = instruction;
 			}
 		}
-
 	}
-	//printMap(&m);
-	//printf("ret val is: %d\n", ret);
+	
 	return ret;
-
 }
+
 
 bool isValid(LLVMOpcode opcode) {
 	switch(opcode) {
@@ -179,68 +182,55 @@ bool constantFold(LLVMBasicBlockRef bb) {
 	long long dummyLHS = 0;
 	for (LLVMValueRef instruction = LLVMGetFirstInstruction(bb); instruction; instruction = LLVMGetNextInstruction(instruction)) {
 		LLVMOpcode Opcode = LLVMGetInstructionOpcode(instruction);
-		switch(Opcode) {
-			case LLVMAdd: {
-				LLVMValueRef valLHS = LLVMGetOperand(instruction, 0);
-				LLVMValueRef valRHS = LLVMGetOperand(instruction, 1);
-				if (LLVMIsConstant(valLHS) && LLVMIsConstant(valRHS)) {
-					dummyLHS = LLVMConstIntGetSExtValue(valLHS);
-					dummyRHS = LLVMConstIntGetSExtValue(valRHS);
-					valLHS = LLVMConstInt(LLVMInt32Type(), dummyLHS, 1);
-					valRHS = LLVMConstInt(LLVMInt32Type(), dummyRHS, 1);
-					LLVMValueRef new_ins = LLVMConstAdd(valLHS, valRHS);
-					LLVMReplaceAllUsesWith(instruction, new_ins);
-					ret = true;
-				} else { 
-					ret = false;
-				}
+		if (Opcode == LLVMAdd) {
 
-				break;
-		      }
-			case LLVMSub:{
-				
-					LLVMValueRef valLHS = LLVMGetOperand(instruction, 0);
-					LLVMValueRef valRHS = LLVMGetOperand(instruction, 1);
-					if (LLVMIsConstant(valLHS) && LLVMIsConstant(valRHS)) {
-						dummyLHS = LLVMConstIntGetSExtValue(valLHS);
-						dummyRHS = LLVMConstIntGetSExtValue(valRHS);
-						valLHS = LLVMConstInt(LLVMInt32Type(), dummyLHS, 1);
-						valRHS = LLVMConstInt(LLVMInt32Type(), dummyRHS, 1);
-						LLVMValueRef new_ins = LLVMConstSub(valLHS, valRHS);
-						LLVMReplaceAllUsesWith(instruction, new_ins);
-						ret = true;
-					} else {
-						ret = false;
-					}
-					break;
-				     }
-			case LLVMMul:{
-					LLVMValueRef valLHS = LLVMGetOperand(instruction, 0);
-					LLVMValueRef valRHS = LLVMGetOperand(instruction, 1);
-					if (LLVMIsConstant(valLHS) && LLVMIsConstant(valRHS)) {
-						dummyLHS = LLVMConstIntGetSExtValue(valLHS);
-						dummyRHS = LLVMConstIntGetSExtValue(valRHS);
-						valLHS = LLVMConstInt(LLVMInt32Type(), dummyLHS, 1);
-						valRHS = LLVMConstInt(LLVMInt32Type(), dummyRHS, 1);
-						LLVMValueRef new_ins = LLVMConstMul(valLHS, valRHS);
-						LLVMReplaceAllUsesWith(instruction, new_ins);
-						ret = true;
-					} else {
-						ret = false;
-					}
-					     
-					break;
-				     }
-			default:
-				     ret = false;
-				break;
+			LLVMValueRef valLHS = LLVMGetOperand(instruction, 0);
+			LLVMValueRef valRHS = LLVMGetOperand(instruction, 1);
+			if (LLVMIsAConstantInt(valLHS) && LLVMIsAConstantInt(valRHS)) {
+				dummyLHS = LLVMConstIntGetSExtValue(valLHS);
+				dummyRHS = LLVMConstIntGetSExtValue(valRHS);
+				valLHS = LLVMConstInt(LLVMInt32Type(), dummyLHS, 1);
+				valRHS = LLVMConstInt(LLVMInt32Type(), dummyRHS, 1);
+				LLVMValueRef new_ins = LLVMConstAdd(valLHS, valRHS);
+				LLVMReplaceAllUsesWith(instruction, new_ins);
+				ret = true;
+			}
+
+		} else if (Opcode == LLVMSub) {
+
+			LLVMValueRef valLHS = LLVMGetOperand(instruction, 0);
+			LLVMValueRef valRHS = LLVMGetOperand(instruction, 1);
+			if (LLVMIsAConstantInt(valLHS) && LLVMIsAConstantInt(valRHS)) {
+				dummyLHS = LLVMConstIntGetSExtValue(valLHS);
+				dummyRHS = LLVMConstIntGetSExtValue(valRHS);
+				valLHS = LLVMConstInt(LLVMInt32Type(), dummyLHS, 1);
+				valRHS = LLVMConstInt(LLVMInt32Type(), dummyRHS, 1);
+				LLVMValueRef new_ins = LLVMConstSub(valLHS, valRHS);
+				LLVMReplaceAllUsesWith(instruction, new_ins);
+				ret = true;
+			}
+
+		} else if (Opcode == LLVMMul) {
+
+
+			LLVMValueRef valLHS = LLVMGetOperand(instruction, 0);
+			LLVMValueRef valRHS = LLVMGetOperand(instruction, 1);
+			if (LLVMIsAConstantInt(valLHS) && LLVMIsAConstantInt(valRHS)) {
+				dummyLHS = LLVMConstIntGetSExtValue(valLHS);
+				dummyRHS = LLVMConstIntGetSExtValue(valRHS);
+				valLHS = LLVMConstInt(LLVMInt32Type(), dummyLHS, 1);
+				valRHS = LLVMConstInt(LLVMInt32Type(), dummyRHS, 1);
+				LLVMValueRef new_ins = LLVMConstMul(valLHS, valRHS);
+				LLVMReplaceAllUsesWith(instruction, new_ins);
+				ret = true;
+			}
 
 
 		}
-		
 
 	}
 
+	printf("ret val: %d\n", ret);
 	return ret;
 
 }
@@ -329,64 +319,91 @@ void kill(std::unordered_map<LLVMBasicBlockRef, std::set<LLVMValueRef>> *genTabl
 bool constantProp(LLVMBasicBlockRef basicBlock, std::unordered_map<LLVMBasicBlockRef, std::set<LLVMValueRef>> *in) {
 	
 	bool ret = false;
-	//printGenTable(&(*in));
-	std::set<LLVMValueRef> R= (*in)[basicBlock];
+	std::set<LLVMValueRef> R = (*in)[basicBlock];  // R = IN[B]
 	std::set<LLVMValueRef> tbd;
+	
 	for(LLVMValueRef instruction = LLVMGetFirstInstruction(basicBlock); instruction; instruction = LLVMGetNextInstruction(instruction)) {
 		LLVMOpcode opcode = LLVMGetInstructionOpcode(instruction);
+		
 		if (opcode == LLVMStore) {
-			if (!R.count(instruction)) {
-				R.insert(instruction);
-			} else {
-				LLVMValueRef op = LLVMGetOperand(instruction, 1);
-				std::set<LLVMValueRef> nR(R);
-				for (auto i : nR) {
-					LLVMValueRef op2 = LLVMGetOperand(i, 1);
-					if (op == op2) {
-						R.erase(i);
+			// Step 1: Add this store to R
+			R.insert(instruction);
+			
+			// Step 2: Remove all stores in R that are killed by this instruction
+			LLVMValueRef store_addr = LLVMGetOperand(instruction, 1);  // Address being stored to
+			std::set<LLVMValueRef> to_remove;
+			
+			for (auto r_inst : R) {
+				if (r_inst == instruction) continue;  // Don't kill yourself
+				
+				if (LLVMGetInstructionOpcode(r_inst) == LLVMStore) {
+					LLVMValueRef r_addr = LLVMGetOperand(r_inst, 1);
+					if (store_addr == r_addr) {
+						to_remove.insert(r_inst);
 					}
 				}
-				R.insert(instruction);
 			}
-
-
+			
+			for (auto rem : to_remove) {
+				R.erase(rem);
+			}
+			
 		} else if (opcode == LLVMLoad) {
-			LLVMValueRef op = LLVMGetOperand(instruction, 0);
-			std::set<long long> cR;
-			bool flag = true;
-			long long val;
-			for (auto j : R) {
-				LLVMValueRef op2 = LLVMGetOperand(j, 1);	
-				LLVMValueRef op1 = LLVMGetOperand(j, 0);
-				if (op2 == op && LLVMIsAConstant(op1)) {
-					val = LLVMConstIntGetSExtValue(op1);
-					cR.insert(val);
-				} else if (op2 == op && !LLVMIsAConstant(op1)) {
-					cR.clear();
-					break;
+			LLVMValueRef load_addr = LLVMGetOperand(instruction, 0);  // Address being loaded from
+			
+			// Find all stores in R that write to this address
+			std::set<LLVMValueRef> reaching_stores;
+			for (auto r_inst : R) {
+				if (LLVMGetInstructionOpcode(r_inst) == LLVMStore) {
+					LLVMValueRef store_addr = LLVMGetOperand(r_inst, 1);
+					if (store_addr == load_addr) {
+						reaching_stores.insert(r_inst);
+					}
 				}
 			}
-
-			if (cR.size() == 1) {
-				LLVMValueRef rep = LLVMConstInt(LLVMInt32Type(), val, 1);
-				LLVMReplaceAllUsesWith(instruction, rep);
-				tbd.insert(instruction);
-				ret = true;
-				flag = true;
-
+			
+			// Check if all reaching stores are constant stores with the same value
+			if (!reaching_stores.empty()) {
+				bool all_constant = true;
+				long long constant_value = 0;
+				bool first = true;
+				
+				for (auto store_inst : reaching_stores) {
+					LLVMValueRef stored_val = LLVMGetOperand(store_inst, 0);
+					
+					if (!LLVMIsConstant(stored_val)) {
+						all_constant = false;
+						break;
+					}
+					
+					long long val = LLVMConstIntGetSExtValue(stored_val);
+					
+					if (first) {
+						constant_value = val;
+						first = false;
+					} else if (val != constant_value) {
+						all_constant = false;
+						break;
+					}
+				}
+				
+				// If all reaching stores write the same constant, propagate it
+				if (all_constant) {
+					LLVMValueRef replacement = LLVMConstInt(LLVMInt32Type(), constant_value, 1);
+					LLVMReplaceAllUsesWith(instruction, replacement);
+					tbd.insert(instruction);
+					ret = true;
+				}
 			}
 		}
 	}
-
-	for (auto k : tbd) {
-		//printf("LOAD 2 DELETE: %s\n", LLVMPrintValueToString(k));
-		LLVMInstructionEraseFromParent(k);
-
+	
+	// Delete marked load instructions
+	for (auto inst : tbd) {
+		LLVMInstructionEraseFromParent(inst);
 	}
-
+	
 	return ret;
-
-
 }
 
 bool cmpB(LLVMValueRef function, std::set<LLVMValueRef> *gSet) {
@@ -437,37 +454,228 @@ bool globalOpt(LLVMValueRef function) {
 		change = false;
 
 		for (LLVMBasicBlockRef basicBlock = LLVMGetFirstBasicBlock(function); basicBlock; basicBlock = LLVMGetNextBasicBlock(basicBlock)) {
+			in[basicBlock].clear();
 			for (auto i : predMap[basicBlock]) {
 				in[basicBlock].insert(out[i].begin(), out[i].end());
 			}
 			std::set<LLVMValueRef> oldout(out[basicBlock]);
 			
-			std::set<LLVMValueRef>ss(in[basicBlock]);
-			std::set<LLVMValueRef>sss(in[basicBlock]);
-			for (auto j : ss) {
-				if (killTable[basicBlock].count(j)) {
-					sss.erase(j);
-				}
+			std::set<LLVMValueRef>temp(in[basicBlock]);
+			for (auto j : killTable[basicBlock]) {
+				temp.erase(j);
 			}
 			out[basicBlock].clear();
 			out[basicBlock].insert(genTable[basicBlock].begin(), genTable[basicBlock].end());
-			out[basicBlock].insert(sss.begin(), sss.end());
+			out[basicBlock].insert(temp.begin(), temp.end());
 
 			if (oldout != out[basicBlock]) {
 				change = true;
 			}
 		}
 	}
+
 	bool opt_change = false;
-	for (LLVMBasicBlockRef basicBlock = LLVMGetFirstBasicBlock(function); basicBlock; basicBlock = LLVMGetNextBasicBlock(basicBlock)) {
-		while(true) {
-			opt_change |= constantFold(basicBlock);
-			opt_change |= constantProp(basicBlock, &in);
+	std::set<bool> bs;
+	while(1) {
+		for (LLVMBasicBlockRef basicBlock = LLVMGetFirstBasicBlock(function); basicBlock; basicBlock = LLVMGetNextBasicBlock(basicBlock)) {
+			//opt_change = false;
+			//opt_change |= constantFold(basicBlock);
+			//opt_change |= constantProp(basicBlock, &in);
+			bs.insert(deadcodeElim(basicBlock));
+			bs.insert(constantFold(basicBlock));
+			bs.insert(constantProp(basicBlock, &in));
 			
 
 			// constant fold
 
 			puts("word");
+			//if (opt_change == false) {
+			//	break;
+			//}
+
+		}
+		if (bs.count(true)) {
+			bs.clear();
+			continue;
+		} else {
+			break;
+		}
+	}
+	
+	if(bs.count(true)) {
+		return true;
+	}
+
+	return false;
+
+
+}
+
+void gen2(LLVMBasicBlockRef bb, std::unordered_map<LLVMBasicBlockRef, std::set<LLVMValueRef>> *genTable, std::set<LLVMValueRef> *iSet, std::unordered_map<LLVMBasicBlockRef, std::set<LLVMBasicBlockRef>> *sucMap, std::set<LLVMValueRef> *gSet) {
+
+	std::set<LLVMValueRef> s;
+	std::set<LLVMBasicBlockRef> ss;
+	std::set<LLVMValueRef> sCheck;
+	(*genTable)[bb] = s;
+	(*sucMap)[bb] = ss;
+
+	for(LLVMValueRef instruction = LLVMGetFirstInstruction(bb); instruction; instruction = LLVMGetNextInstruction(instruction)) {
+		(*gSet).insert(instruction);
+		LLVMOpcode op = LLVMGetInstructionOpcode(instruction);
+		if (op == LLVMStore) {
+			LLVMValueRef operand = LLVMGetOperand(instruction, 1);
+			if (sCheck.count(operand)) {
+				continue;
+			} else {
+				sCheck.insert(operand);
+			}
+		} else if (op == LLVMLoad) {
+			(*iSet).insert(instruction);
+			LLVMValueRef operand = LLVMGetOperand(instruction, 0);
+			if (sCheck.count(operand)) {
+				// don't add to gen set
+				continue;
+			} else {
+				(*genTable)[bb].insert(instruction);
+			}
+		} else if (LLVMIsATerminatorInst(instruction)) {
+			unsigned int suc_num = LLVMGetNumSuccessors(instruction);
+			for (unsigned int i = 0; i < suc_num; i++) {
+				LLVMBasicBlockRef bnew = LLVMGetSuccessor(instruction, i);
+				(*sucMap)[bb].insert(bnew);
+			}
+
+		}
+
+	}
+}	
+
+void kill2(std::unordered_map<LLVMBasicBlockRef, std::set<LLVMValueRef>> *genTable, std::unordered_map<LLVMBasicBlockRef, std::set<LLVMValueRef>> *killTable, std::set<LLVMValueRef> *iSet) {
+
+	for (auto i : (*genTable)) {
+		std::set<LLVMValueRef> s;
+		(*killTable)[i.first] = s;
+		for (LLVMValueRef instruction = LLVMGetFirstInstruction(i.first); instruction; instruction = LLVMGetNextInstruction(instruction)) {
+
+			LLVMOpcode opcode = LLVMGetInstructionOpcode(instruction);
+			if (opcode == LLVMStore) {
+				LLVMValueRef storeOpt1 = LLVMGetOperand(instruction, 1);
+				for (auto k : (*iSet)) {
+					LLVMValueRef loadOpt0 = LLVMGetOperand(k, 0);
+					if (storeOpt1 == loadOpt0) {
+						(*killTable)[i.first].insert(k);
+					}
+				}
+			}
+		}
+
+	}
+
+}
+
+
+bool storeElim(LLVMBasicBlockRef basicBlock, std::unordered_map<LLVMBasicBlockRef, std::set<LLVMValueRef>> *out) {
+	
+	bool ret = false;
+	//printGenTable(&(*in));
+	std::set<LLVMValueRef> R= (*out)[basicBlock];
+	std::set<LLVMValueRef> tbd;
+	for(LLVMValueRef instruction = LLVMGetFirstInstruction(basicBlock); instruction; instruction = LLVMGetNextInstruction(instruction)) {
+		LLVMOpcode opcode = LLVMGetInstructionOpcode(instruction);
+		bool flag = true;
+
+		if (opcode == LLVMStore) {
+			LLVMValueRef op = LLVMGetOperand(instruction, 1);
+			std::set<LLVMValueRef> s;
+
+			for (auto i : R) {
+				LLVMValueRef loadOp = LLVMGetOperand(instruction, 0);
+				if (loadOp == op) {
+					//s.insert(loadOp);
+					//s.clear();
+					flag = false;
+					break;
+				}
+
+			}
+			if (flag == false) {
+				puts("Store has no referencded lod ptr");
+				ret = true;
+				tbd.insert(instruction);
+			}
+
+		}
+
+	}
+
+	for(auto i : tbd) {
+		LLVMInstructionEraseFromParent(i);
+	}
+
+	return ret;
+
+
+}	
+
+bool livevarExp(LLVMValueRef function) {
+
+	std::unordered_map<LLVMBasicBlockRef, std::set<LLVMBasicBlockRef>> sucMap;
+	std::unordered_map<LLVMBasicBlockRef, std::set<LLVMValueRef>> genTable;
+	std::unordered_map<LLVMBasicBlockRef, std::set<LLVMValueRef>> killTable;
+	std::set<LLVMValueRef> iSet;
+	std::set<LLVMValueRef> gSet;
+
+	//std::unordered_map<LLVMBasicBlockRef, std::set<LLVMValueRef>> in;
+	std::unordered_map<LLVMBasicBlockRef, std::set<LLVMValueRef>> out;
+
+	for (LLVMBasicBlockRef basicBlock = LLVMGetFirstBasicBlock(function); basicBlock; basicBlock = LLVMGetNextBasicBlock(basicBlock)) {
+		// gen set
+		gen2(basicBlock, &genTable, &iSet, &sucMap, &gSet);
+		std::set<LLVMValueRef> s;
+		out[basicBlock] = s;
+	}
+
+	kill2(&genTable, &killTable, &iSet);
+
+	std::unordered_map<LLVMBasicBlockRef, std::set<LLVMValueRef>> in(genTable);
+
+
+	bool change = true;
+	while (change) {
+		
+		change = false;
+
+		for (LLVMBasicBlockRef basicBlock = LLVMGetFirstBasicBlock(function); basicBlock; basicBlock = LLVMGetNextBasicBlock(basicBlock)) {
+			for (auto i : sucMap[basicBlock]) {
+				out[basicBlock].insert(in[i].begin(), in[i].end());
+			}
+			std::set<LLVMValueRef> oldout(in[basicBlock]);
+			
+			std::set<LLVMValueRef>ss(out[basicBlock]);
+			std::set<LLVMValueRef>sss(out[basicBlock]);
+			for (auto j : ss) {
+				if (killTable[basicBlock].count(j)) {
+					sss.erase(j);
+				}
+			}
+			in[basicBlock].clear();
+			in[basicBlock].insert(genTable[basicBlock].begin(), genTable[basicBlock].end());
+			in[basicBlock].insert(sss.begin(), sss.end());
+
+			if (oldout != in[basicBlock]) {
+				change = true;
+			}
+		}
+	}
+
+	printGenTable(&out);
+
+	bool opt_change = false;
+	for (LLVMBasicBlockRef basicBlock = LLVMGetFirstBasicBlock(function); basicBlock; basicBlock = LLVMGetNextBasicBlock(basicBlock)) {
+		while(true) {
+
+			opt_change |= storeElim(basicBlock, &out);
+			
 			if (!opt_change) {
 				break;
 			}
@@ -499,15 +707,16 @@ void walkBasicblocks(LLVMValueRef function) {
 			}
 
 		}
-		
+		glob_change = false;		
 		glob_change |= globalOpt(function);
 		puts("checking");
 		
-		if (!glob_change) {
+		if (glob_change == false) {
 			break;
 		}
-		glob_change = false;
 	}
+
+	//livevarExp(function);
 	
 }
 
